7de45dd26ec65ce8d471fcc7077456703bad79aa
Marko Stankovic <smankovic@gmail.com>
2025-11-02 19:20:24 +0100

    Add Tutorial Walkthrough

--- Assets/_Project/Scripts/Managers/UIButtonManager.cs
+++ Assets/_Project/Scripts/Managers/UIButtonManager.cs
@@ -12,6 +12,48 @@ namespace Managers
             UIManager.Instance.LoadSceneWithLoadingScreen("MainLevel");
         }
 
+        public void OnFinishButtonClicked()
+        {
+            UIManager.Instance.HideAllPanels();
+            UIManager.Instance.ShowPanel("MainMenu");
+        }
+
+        public void OnTutorial_1_ButtonClicked()
+        {
+            UIManager.Instance.HideAllPanels();
+            UIManager.Instance.ShowPanel("Tutorial_Menu_1");
+        }
+
+        public void OnTutorial_2_ButtonClicked()
+        {
+            UIManager.Instance.HideAllPanels();
+            UIManager.Instance.ShowPanel("Tutorial_Menu_2");
+        }
+        
+        public void OnTutorial_3_ButtonClicked()
+        {
+            UIManager.Instance.HideAllPanels();
+            UIManager.Instance.ShowPanel("Tutorial_Menu_3");
+        }
+        
+        public void OnTutorial_4_ButtonClicked()
+        {
+            UIManager.Instance.HideAllPanels();
+            UIManager.Instance.ShowPanel("Tutorial_Menu_4");
+        }
+        
+        public void OnTutorial_5_ButtonClicked()
+        {
+            UIManager.Instance.HideAllPanels();
+            UIManager.Instance.ShowPanel("Tutorial_Menu_5");
+        }
+        
+        public void OnTutorial_6_ButtonClicked()
+        {
+            UIManager.Instance.HideAllPanels();
+            UIManager.Instance.ShowPanel("Tutorial_Menu_6");
+        }
+
         public void OnQuitButtonClicked()
         {
 #if UNITY_EDITOR
